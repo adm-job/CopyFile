b298106c6c44f6c024861b01f640a8673f8d1295
Stas <adm.job@yandex.ru>
2024-10-02 17:53:46 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CopyFile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Получение названия файла для копирования
+        string? nameFile = "settings.txt";
+        string? nameFileFlg = "settings.flg";
+
+        string patch = Directory.GetCurrentDirectory();
+        Path.GetDirectoryName(patch);
+        string filePatch =  patch + "\\" + nameFile;
+        string fileFlgPatch = patch + "\\" + nameFileFlg;
+
+        if (!File.Exists(filePatch)){
+            Console.WriteLine($"Файл {nameFile} не найден");
+            //break;
+        }
+
+        Console.Clear();
+        Console.WriteLine("Название файла конфигурации: " + nameFile);
+        Console.WriteLine("Файл находится: " + filePatch);
+        Console.WriteLine("Название файла флага: " + nameFileFlg);
+        Console.WriteLine("Файл находится: " + fileFlgPatch);
+
+
+        string[] paths = { @"d:\archives", "2001", "media", "images" };
+        string fullPath = Path.Combine(paths);
+        Console.WriteLine(fullPath);
+        Console.ReadLine();
+
+    }
+}
