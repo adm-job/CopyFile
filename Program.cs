886f2e18273c6f3d7258598e86f98ec4c8118ccb
Stas <adm.job@yandex.ru>
2024-10-11 16:36:44 +0500

    comment all

--- Program.cs
+++ Program.cs
@@ -9,28 +9,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Получение названия файла для копирования
-        string? nameFile = "settings.txt";
-        string? nameFileFlg = "settings.flg";
+/*         //Получение названия файла для копирования
+        string? nameFile = "settingstest.txt";
+        string? nameFileFlg = "settingstest.flg";
+        string? nameFileList = "listPC.txt";
+        //List<string> connectList = new List<string>();
 
         string patch = Directory.GetCurrentDirectory();
-        Path.GetDirectoryName(patch);
         string filePatch = patch + "\\" + nameFile;
         string fileFlgPatch = patch + "\\" + nameFileFlg;
+        string fileListPatch = patch + "\\" + nameFileList;
+
+        var file1 = new FileInfo(filePatch);
+        var file2 = new FileInfo(fileFlgPatch);
+
+
 
         Console.Clear();
 
         if (!File.Exists(filePatch))
         {
             Console.WriteLine($"Файл {nameFile} не найден");
-            //break;
+        }
+        else if (!File.Exists(fileFlgPatch))
+        {
+            Console.WriteLine($"Файл {fileFlgPatch} не найден");
         }
         else
         {
             Console.WriteLine("Название файла конфигурации: " + nameFile);
-            Console.WriteLine("Файл находится: " + filePatch+ "\n");
+            Console.WriteLine("Файл находится: " + filePatch + "\n");
             Console.WriteLine("Название файла флага: " + nameFileFlg);
             Console.WriteLine("Файл находится: " + fileFlgPatch);
+        }
+        foreach (var line in File.ReadLines(fileListPatch))
+        {
+            Console.WriteLine($"{line}");
+            
+            file1.CopyTo(line,true);
+           // file2.CopyTo(nameFileFlg,true);
+        } */
+
+        string path = @"d:\in\SoureFile.txt";
+        string path2 = @"d:\in\NewFile.txt";
+        FileInfo fi1 = new FileInfo(path);
+        //FileInfo fi2 = new FileInfo(path2);
+
+        try
+        {
+            // Create the source file.
+           // using (FileStream fs = fi1.Create()) { }
+
+            //Ensure that the target file does not exist.
+/*             if (File.Exists(path2))
+            {
+                fi2.Delete();
+            } */
+
+            //Copy the file.f
+            fi1.CopyTo(path2,true);
+            Console.WriteLine("{0} was copied to {1}.", path, path2);
+        }
+        catch (IOException ioex)
+        {
+            Console.WriteLine(ioex.Message);
         }
 
     }
